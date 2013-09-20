2c11657df465c7945315a8a4264be39e958fc30d
CLYwBless <andyng@hawaii.edu>
2013-09-19 17:26:45 -1000

    Changed layout to be Linear

--- src/com/example/scheduler/View_schedule.java
+++ src/com/example/scheduler/View_schedule.java
@@ -1,10 +1,30 @@
 package com.example.scheduler;
 
+/* Action Bar Sherlock Imports*/
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.ActionMode;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.SubMenu;
+
+/* Basic Android Imports*/
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class View_schedule extends Activity {
+
+public class View_schedule extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +35,7 @@ public class View_schedule extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.view_schedule, menu);
+		getSupportMenuInflater().inflate(R.menu.view_schedule, menu);
 		return true;
 	}
 
