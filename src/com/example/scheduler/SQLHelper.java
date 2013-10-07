42cb83f2c8453a35538769df87319466636c0cc0
CLYwBless <andyng@hawaii.edu>
2013-10-06 19:01:19 -1000

    Modifications to SQLHelper

--- src/com/example/scheduler/SQLHelper.java
+++ src/com/example/scheduler/SQLHelper.java
@@ -5,14 +5,30 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class SQLHelper extends SQLiteOpenHelper {
-
+	
+	/* Class used for Getting Email Data*/
+	private final Email_Fetcher e_f = new Email_Fetcher();
+	private String identifier;
+	
 	/* This is just a Skeleton Class, Needs to be Updated */
-    private static final int DATABASE_VERSION = 2;
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_DESC = "desc";
+	public static final String COLUMN_ALARM = "alarm";
+	public static final String COLUMN_MONTH = "month";
+	public static final String COLUMN_DAY = "day";
+	public static final String COLUMN_YEAR = "year";
+	public static final String COLUMN_START = "start_time";
+	public static final String COLUNMN_END = "end_time";
+	
+	
+    
+	private static final int DATABASE_VERSION = 2;
     private static final String TABLE_NAME = "USREVENTS";
-	private static final String KEY_WORD = null;
-	private static final String KEY_DEFINITION = null;
-	private static final String DATABASE_NAME = null;
-
+	private final static String DATABASE_NAME = "Events.db";
+	
+	
+	
     SQLHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
