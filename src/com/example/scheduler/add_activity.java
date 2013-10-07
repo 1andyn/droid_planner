71d91596b1fc0cbd8787bca20e593e08ac5a6e5a
CLYwBless <andyng@hawaii.edu>
2013-10-06 16:42:55 -1000

    Completed Skeleton class for add_activity

--- src/com/example/scheduler/add_activity.java
+++ src/com/example/scheduler/add_activity.java
@@ -3,15 +3,29 @@ package com.example.scheduler;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Window;
 
-import edu.calpoly.android.lab3.Joke;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 public class Add_activity  extends SherlockFragmentActivity {
 	/* Necessary Data for Resources */
+	protected EditText name_et;
+	protected EditText desc_et;
+	
+	/* Time Based Data Resources*/
+	protected TimePicker start_tp;
+	protected TimePicker end_tp;
+	protected DatePicker r_dp;
+	
+	/* Button Based Resources*/
+	protected ToggleButton alarm_tb;
+	protected Button creation_b;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,25 +44,26 @@ public class Add_activity  extends SherlockFragmentActivity {
 	/* ActionBar Configuration */
 	protected void config_actionbar()
 	{
-//		ActionBar ab = getSupportActionBar();
-//		ab.setDisplayShowTitleEnabled(false); 
-//		ab.setDisplayShowHomeEnabled(false);
-		
-		/* Will re-implement last
-		LayoutInflater inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	    View v =inflater.inflate(R.layout.top_bar_view, null, true);
-	    ab.setCustomView(v);
-		ab.setDisplayShowCustomEnabled(true);
-		
+		/*
+		 *  Functionality is not necessary but may make app look nicer later on
+		 * 
+		 * ActionBar ab = getSupportActionBar();
+		 * ab.setDisplayShowTitleEnabled(false); 
+		 * ab.setDisplayShowHomeEnabled(false);
+		 * LayoutInflater inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	     * View v =inflater.inflate(R.layout.top_bar_view, null, true);
+	     * ab.setCustomView(v);
+		 * ab.setDisplayShowCustomEnabled(true);
 		*/
 	}
 	
 	protected void initAddJokeListeners() {
-		button.setOnClickListener(new OnClickListener() {
+		creation_b.setOnClickListener(new OnClickListener() {
 		
 			@Override
 			public void onClick(View view) {
-			if(edittext.getText().toString().trim().length() > 0)
+				/* Checks for Empty name as it is a Requirement */
+			if(name_et.getText().toString().trim().length() > 0)
 			{
 				Event temp = new Event();
 //				temp.setJoke(m_vwJokeEditText.getText().toString());
