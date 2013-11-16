cab1ff97de8d30f7da749e1e26dcdf27effde257
CLYwBless <andyng@hawaii.edu>
2013-11-15 17:23:37 -1000

    Adding code from test driver alarm to primary application alarm
    Signed-off-by: CLYwBless <andyng@hawaii.edu>

--- src/com/example/scheduler/Alarm_Module.java
+++ src/com/example/scheduler/Alarm_Module.java
@@ -5,50 +5,90 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.IBinder;
 
 public class Alarm_Module extends Service{
 	
-	private NotificationManager mManager;
+	private Bitmap icon;
+	private Uri sound;
+	private int ledinterval = 1000; // Milliseconds
+	private String ev_name = Default_Name; // Event name
+	private String desc_name = Default_Desc; // Desc name
+	private final static String note_tick = "Planner Plus Notification!";
+	private final static String Default_Name = "DEFAULT EVENT NAME";
+	private final static String Default_Desc = "DEFAULT DESCRIPTION TEXT";
+	private int ledcolor = 0xff0000ff; // LED Color for Notification (will become Dynamic
+	private final long[] pattern = {100, 100, 100, 100, 100, 100}; // Vibrate Pattern
 	
-	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	 @Override
-	    public void onCreate() 
-	    {
-	       // TODO Auto-generated method stub  
-	       super.onCreate();
-	    }
-	 
-	   @SuppressWarnings("static-access")
-	   @Override
-	   public int onStartCommand(Intent intent,int flags, int startId)
-	   {
-	       super.onStartCommand(intent, flags, startId);
-	      
-	       mManager = (NotificationManager) this.getApplicationContext().getSystemService(this.getApplicationContext().NOTIFICATION_SERVICE);
-	       Intent intent1 = new Intent(this.getApplicationContext(),Schedule.class);
-	     
-	       Notification notification = new Notification(R.drawable.ic_action_event,"TEST ALARM", System.currentTimeMillis());
-	       intent1.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP| Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	 
-	       PendingIntent pendingNotificationIntent = PendingIntent.getActivity( this.getApplicationContext(),0, intent1,PendingIntent.FLAG_UPDATE_CURRENT);
-	       notification.flags |= Notification.FLAG_AUTO_CANCEL;
-	       notification.setLatestEventInfo(this.getApplicationContext(), "AlarmManagerDemo", "This is a test Alarm Alert.", pendingNotificationIntent);
-	 
-	       mManager.notify(0, notification);
-	       return 0;
-	    }
-	 
-	    @Override
-	    public void onDestroy() 
-	    {
-	        // TODO Auto-generated method stub
-	        super.onDestroy();
-	    }
+     private NotificationManager mManager;
 
+     @Override
+     public IBinder onBind(Intent arg0)
+     {
+        return null;
+     }
+
+    @Override
+    public void onCreate() 
+    {
+    	initialization();
+    }
+   
+    /* Function for initializing Variables */
+   private void initialization()
+   {
+	   icon = BitmapFactory.decodeResource(this.getApplicationContext().getResources(),
+               R.drawable.notelarge);
+	   sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+   }
+    
+   @SuppressWarnings("static-access")
+   @Override
+   public int onStartCommand(Intent intent, int flags, int startId)
+   {
+       super.onStartCommand(intent, flags, startId);
+     
+	   Intent OpenIntent = new Intent(this, Schedule.class);
+	   OpenIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP| Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	   /* Pending Intent is for intent that is triggered upon notification click */
+	   PendingIntent pendingNoteIntent = PendingIntent.getActivity(this, 0, OpenIntent, 0);
+
+	   /* Notification Creation Code*/
+	   Notification notification = new Notification.Builder(getApplicationContext())
+       .setContentTitle(ev_name) // TITLE OF NOTIFICATION
+       .setContentText(desc_name) // BODY TEXT OF NOTIFICATION
+       .setTicker(note_tick) // MESSAGE TO USER during NOTIFICATION (SMALL TEXT)
+       .setSmallIcon(R.drawable.ic_event_small) // SMALLER NOTIFICATION ICON
+       .setLargeIcon(icon) // LARGER NOTIFICATION ICON
+       .setSound(sound) // SET SOUND
+       .setVibrate(pattern) // SET VIBRATE PATTERN
+       .setLights(ledcolor, ledinterval, ledinterval) // SET LED SETTINGS, LEDCOLOR, DURATION LED ON, DURATION OFF
+       .setContentIntent(pendingNoteIntent) // SET INTENT FOR RETURNING TO APPLICATION WHEN NOTIFICATION IS PRESSED
+       .build();
+       mManager = (NotificationManager) this.getApplicationContext().getSystemService(this.NOTIFICATION_SERVICE);
+
+	   // Code that Hides Notification after it is selected
+       notification.flags |= Notification.FLAG_AUTO_CANCEL;
+	   
+       mManager.notify(0, notification);
+       return super.onStartCommand(intent, flags, startId);
+    }
+   
+    @Override
+    public void onDestroy() 
+    {
+        super.onDestroy();
+    }
+
+    /* Used for setting name/body texts outside of this AlarmServiceModule */
+    public void setNameDesc(String name, String desc)
+    {
+    	ev_name = name; // Event name
+    	desc_name = desc; // Desc name
+    }
+    
 }
