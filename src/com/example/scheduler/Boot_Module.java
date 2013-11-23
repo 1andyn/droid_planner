7a8a228fd77dfcff570b6d110c803960310c5173
CLYwBless <andyng@hawaii.edu>
2013-11-22 22:03:21 -1000

    Implementing new code in attempt to restore alarms after reboot

---
+++ src/com/example/scheduler/Boot_Module.java
@@ -0,0 +1,13 @@
+package com.example.scheduler;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Boot_Module extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Do your stuff
+    }
+}
