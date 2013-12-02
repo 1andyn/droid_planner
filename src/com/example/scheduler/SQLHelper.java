56c821a4632217e7506c3e3f3d864eccbb8c5b5c
CLYwBless <andyng@hawaii.edu>
2013-12-02 12:11:27 -1000

    added code to create additional table

--- src/com/example/scheduler/SQLHelper.java
+++ src/com/example/scheduler/SQLHelper.java
@@ -20,9 +20,13 @@ public class SQLHelper extends SQLiteOpenHelper {
 	public static final String COLUMN_COLOR = "color";
 	public static final String COLUMN_REP = "repetition";
 	public static final String COLUMN_ASEC = "alarm_second";
-	 
+	
+	public static final String COLUMN_EVENT_ID = "e_id";
+	public static final String COLUMN_OBJECT_ID = "obj_id";
+	
 	private static final int DATABASE_VERSION = 2;
     public static final String TABLE_NAME = "USREVENTS";
+    public static final String OBJECT_TABLE_NAME = "OBJTABLE";
 	private final static String DATABASE_NAME = "Events.db";
 		
 	private static final String DATABASE_CREATE = "CREATE TABLE "
@@ -40,6 +44,12 @@ public class SQLHelper extends SQLiteOpenHelper {
 		      +	COLUMN_REP + " TEXT NOT NULL,"
 		      + COLUMN_ASEC + " BIGINT " + " );";
 	
+	private static final String OBJECT_TABLE_CREATE = "CREATE TABLE "
+		      + OBJECT_TABLE_NAME
+		      + "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+		      + COLUMN_EVENT_ID + " BIGINT NOT NULL, "
+		      +	COLUMN_OBJECT_ID + " TEXT NOT NULL," + " );";
+	
 	  public SQLHelper(Context context) {
 		    super(context, DATABASE_NAME, null, DATABASE_VERSION);
 		    //super(context, "/mnt/sdcard/" + DATABASE_NAME, null, DATABASE_VERSION);
@@ -48,6 +58,7 @@ public class SQLHelper extends SQLiteOpenHelper {
 	  @Override
 	  public void onCreate(SQLiteDatabase database) {
 	    database.execSQL(DATABASE_CREATE);
+	    database.execSQL(OBJECT_TABLE_CREATE);
 	  }
 	
 	  @Override
@@ -56,6 +67,7 @@ public class SQLHelper extends SQLiteOpenHelper {
 	        "Upgrading database from version " + oldVersion + " to "
 	            + newVersion + ", which will destroy all old data");
 	    db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+	    db.execSQL("DROP TABLE IF EXISTS " + OBJECT_TABLE_NAME);
 	    onCreate(db);
 	  }
 }
