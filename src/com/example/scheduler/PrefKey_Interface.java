763da83d5534ea385bbf89f42394e200cc9e271a
CLYwBless <andyng@hawaii.edu>
2013-12-02 13:24:54 -1000

    added additional key to prefkey interface

--- src/com/example/scheduler/PrefKey_Interface.java
+++ src/com/example/scheduler/PrefKey_Interface.java
@@ -4,4 +4,5 @@ public interface PrefKey_Interface {
 	final static String app_id = "com.example.scheduler";
     final static String usr_email = "pref_email";
     String null_email = "john@doe.com";
+    String db_version = "database_version";
 }
