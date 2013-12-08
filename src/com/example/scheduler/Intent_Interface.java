64cba170f27bd69e8672fbea39b65722b42bbefc
CLYwBless <andyng@hawaii.edu>
2013-12-07 14:17:32 -1000

    added new cases to interface

--- src/com/example/scheduler/Intent_Interface.java
+++ src/com/example/scheduler/Intent_Interface.java
@@ -4,6 +4,7 @@ public interface Intent_Interface {
 	
 	/* Intent Codes */
 	final static int month_REQUESTCODE = 1;
+	final static int week_REQUESTCODE = 2;
 	final static int REQUEST_CANCELLED = -1;
 	final static int RESULT_OKAY = 1;
 	final static String SCHEDULE_DAY = "S_DAY";
@@ -11,4 +12,6 @@ public interface Intent_Interface {
 	final static String SCHEDULE_YEAR = "S_YR";
 	final static String SELECT_KEY = "CURRENT_DATE";
 	final static String SELECT_ID_KEY = "SELECT_ID";
+	final static String MONTH_KEY = "MONTH_FROM_WEEK";
+	final static String ADD_KEY = "ADD_FROM_WEEK";
 }
